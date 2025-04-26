13a2a0d422b2218833128deae5895bab581c448b
Nishit <nishitku123@gmail.com>
2025-04-27 01:17:19 +0530

    some minor changges to run

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar.jsx';
 import Signup from "./pages/signup.jsx";
 import Login from "./pages/login.jsx";
